ba2f7e8b9d8eab15f7ac35145a65770de02ee78e
Juan David Ome <104031677+Juandavidome@users.noreply.github.com>
2022-05-17 13:38:07 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,128 +6,210 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    internal class Class1
+    internal class Program
     {
+        public static double n1, n2, n3, n4;
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Menu Principal");
-            Console.WriteLine("Seleccione el metodo: \n1. Operadores  \n2. Condicionales");
-
-            String menu = null;
-            menu = Console.ReadLine();
-
-            switch (menu)
+            string linea;
+            do
             {
-                case "1":
-                    Console.WriteLine("\n1. OPERADORES");
+
+                linea = null;
+
+                Console.WriteLine("Menu Principal");
+                Console.WriteLine("Seleccione el metodo: \n1. Operadores  \n2. Condicionales \n3. Ciclos \n4. Salida");
+
+                String menu = null;
+                menu = Console.ReadLine();
+
+                switch (menu)
+                {
+                    case "1":
+                        Console.WriteLine("\n1. OPERADORES");
 
 
-                    Console.WriteLine("Seleccione el tipo de operacion: \n1. Area de un Triangulo " + "\n2. Suma " + "\n3. Numero elevado al cuadrado " + "\n4. Euros a Dolares" + "\n5. Calcular Area y Perimetro de un Cuadrado" + "\n6. Determinar Area y Volumen de un Cilindro" + "\n7. Determinar Longitud de la Circunferencia y Area del Circulo" + "\n8. Promedio de tres numeros ");
-                    String Operadores = null;
-                    Operadores = Console.ReadLine();
-                    switch (Operadores)
-                    {
-                        case "1":
-                            {
-                                Console.WriteLine("Calcular Area de un Triangulo");
-                                Console.WriteLine("Ingrese dos valores enteros");
-                                Console.WriteLine("Ingrese el valor de la base: ");
-                                double base_t = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("Ingrese el valor de la altura: ");
-                                double altura = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("El Area del triangulo es " + ((base_t * altura) / 2));
+                        Console.WriteLine("Seleccione el tipo de operacion: \n1. Area de un Triangulo " + "\n2. Suma " + "\n3. Numero elevado al cuadrado " + "\n4. Euros a Dolares" + "\n5. Calcular Area y Perimetro de un Cuadrado" + "\n6. Determinar Area y Volumen de un Cilindro" + "\n7. Determinar Longitud de la Circunferencia y Area del Circulo" + "\n8. Promedio de tres numeros ");
+                        String Operadores = null;
+                        Operadores = Console.ReadLine();
+                        switch (Operadores)
+                        {
+                            case "1":
+                                {
+                                    Ejercicios.AreaTriangulo();
+                                    break;
+                                }
+
+                            case "2":
+                                {
+                                    Ejercicios.SUMA();
+                                    break;
+                                }
+
+                            case "3":
+                                {
+                                   Ejercicios.Numeroalcuadrado();
+                                    break;
+                                }
+
+                            case "4":
+                                {
+                                    Ejercicios.EurosDolares();
+                                    break;
+                                }
+
+                            case "5":
+                                {
+                                    Ejercicios.AreaCuadrado();
+                                    break;
+                                }
+
+                            case "6":
+                                {
+                                    Ejercicios.Areayvolumen();
+                                    break;
+                                }
+                            case "7":
+                                {
+                                    Ejercicios.Longitudyarea();
+
+                                    break;
+                                }
+                            case "8":
+                                {
+                                    Ejercicios.Promedio();
+                                    break;
+                                }
+
+                            default:
+                                Console.WriteLine("Error la opcion no existe, regresando al menu.");
                                 break;
-                            }
+                        }
 
-                        case "2":
+                        break;
+
+                    case "2":
+                        {
+                            Console.WriteLine("\n2. CONDICIONALES");
+                            Console.WriteLine("");
+                            Console.WriteLine("\n1. Numero Positivo o Negativo \n2. Numero mayor y menor \n3. Ingresar tres numeros positivos para saber cual es mayor y menor \n4. Dos números A y B, sumarlos si A es menor que B o sino restarlos \n5. Dos números A y B encontrar el cociente \n6. Dos números sumarlos si alguno es negativo, sino multiplicarlos  \n7. Determinar si un año es bisiesto o no");
+                            String Condicionales = null;
+                            Condicionales = Console.ReadLine();
+                            switch (Condicionales)
                             {
-                                Console.WriteLine("Suma");
-                                Console.WriteLine("Ingrese dos valores enteros");
-                                Console.WriteLine("Ingrese el primer valor:");
-                                double num3 = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("Ingrese el segundo valor: ");
-                                double num4 = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("El resultado de la suma es: " + (num3 + num4));
-                                break;
-                            }
+                                case "1":
+                                    {
+                                        Ejercicios.PosioNega();
+                                        break;
+                                    }
 
-                        case "3":
-                            {
-                                Console.WriteLine("Numero elevado al Cuadrado");
-                                Console.WriteLine("Ingrese el valor:");
-                                double elevado = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("El numero elevado al cuadrado es: " + (elevado * elevado));
-                                break;
-                            }
+                                case "2":
+                                    {
+                                        Ejercicios.MayorOMenor();
+                                        break;
+                                    }
+                                case "3":
+                                    {
+                                        Ejercicios.TresNum();                                    
+                                        break;
+                                    }
+                                case "4":
+                                    {
+                                        Ejercicios.AyB();
+                                        
+                                        break;
+                                    }
+                                case "5":
+                                    {
+                                        Ejercicios.AyB2();
+                                        break;
+                                    }
+                                case "6":
+                                    {
+                                        Ejercicios.Dosnumeros();
+                                        break;
+                                    }
+                                case "7":
+                                    {
+                                        Ejercicios.Añobisiesto();   
 
-                        case "4":
-                            {
-                                Console.WriteLine("Euros a Dolares");
-                                Console.WriteLine("Ingrese el valor que quiera convertir: ");
-                                double Convertido = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("El resultado de la conversion es: " + (Convertido * 1.06) + " USD $");
-                                break;
+                                        break;
+                                    }
+                                default:
+                                    Console.WriteLine("Error la opcion no existe, regresando al menu.");
+                                    break;
                             }
-
-                        case "5":
-                            {
-                                Console.WriteLine("Calcular Area y Perimetro de un Cuadrado");
-                                Console.WriteLine("Inserte el valor del lado de un Cuadrado");
-                                double cuadrado = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("El resultado del area es: " + (cuadrado * cuadrado));
-                                Console.WriteLine("El resultado del perimetro es: " + (cuadrado * 4));
-                                break;
-                            }
-
-                        case "6":
-                            {
-                                Console.WriteLine("Determinar Area y Volumen de un Cilindro");
-                                Console.WriteLine("");
-                                Console.WriteLine("Ingrese el valor del Radio del Cilindro: ");
-                                int radio = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("Ingrese el valor de la Altura del Cilindro: ");
-                                int altura = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("El resultado del Area del cilindro es: " + ((2 * 3.1416) * (radio * altura) + (2 * 3.1416) * (radio * radio)));
-                                Console.WriteLine("El resultado del Volumen del cilindro es: " + ((radio * radio * 3.1416) * (altura)));
-                                break;
-                            }
-                        case "7":
-                            {
-                                Console.WriteLine("Determinar la Longitud y Area de un Circulo");
-                                Console.WriteLine("");
-
-                                Console.WriteLine("Ingrese el valor del Radio del Cilindro: ");
-                                double radio = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("El resultado de la longitud de la Circunferencia es: " + ((2 * 3.1416) * (radio)));
-                                Console.WriteLine("El resultado del Area del Circulo es: " + ((3.1416) * (radio * radio)));
-
-                                break;
-                            }
-                        case "8":
-                            {
-                                Console.WriteLine("Promedio de tres numeros");
-                                Console.WriteLine("Ingrese el primer valor: ");
-                                int numero1 = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("Ingrese el segundo valor: ");
-                                int numero2 = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("Ingrese el tercer valor: ");
-                                int numero3 = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine("La suma de los numeros es: " + (numero1 + numero2 + numero3));
-                                Console.WriteLine("El promedio de los numeros es: " + (numero1 + numero2 + numero3) / (3));
-                                break;
-                            }
-
-                        default:
-                            Console.WriteLine("Error, la opcion no existe");
                             break;
-                    }
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("\n3. Ciclos");
+                            Console.WriteLine("Seleccione el tipo de operacion: \n1. Todos los multiplos de 3, entre 1 a 100 \n2. Numeros impares entre 0 a 100 \n3. Numeros pares entre 1 a 100 \n4. Numeros cuadrados del 1 al 30 \n5. Suma de los cuadrados de los cien primeros numeros naturales \n6. Dados dos números naturales, mostrar todos los números comprendidos entre ellos en secuencia ascendente  \n7.Suma de todos los numeros digitados hasta el ingreso de 0");
+                            String Ciclos = null;
+                            Ciclos = Console.ReadLine();
+                            switch (Ciclos)
+                            {
+                                case "1":
+                                    {
+                                        Ejercicios.Multi3();
+                                        break;
+                                    }
+                                case "2":
+                                    {
+                                        Ejercicios.NumImp();
+                                        break;
+                                    }
+                                case "3":
+                                    {
+                                        Ejercicios.NumPar();
+                                        break;
+                                    }
+                                case "4":
+                                    {
+                                        Ejercicios.NumCuad();
+                                        break;
+                                    }
+                                case "5":
+                                    {
+                                        Ejercicios.SumaCuadra();
+                                        break;
+                                    }
+                                case "6":
+                                    {
+                                       Ejercicios.NumerosNatu();    
 
-                    break;
+                                        break;
+                                    }
+                                case "7":
+                                    {
+                                        Ejercicios.SumaTodo();  
 
-            }
+                                        break;
+                                    }
+                                default:
+                                    Console.WriteLine("Error la opcion no existe, regresando al menu.");
+                                    break;
+                            }
 
+                            break;
 
+                        }
+
+                    case "4":
+                        {
+                            Console.WriteLine("Esta saliendo del programa, se reiniciara. ");
+                            break;
+                        }
+
+                    default:
+                        Console.WriteLine("Error la opcion no existe, regresando al menu.");
+                        break;
+                }
+                Console.WriteLine("---------------------------------------------------------------------------------------------");
+                Console.WriteLine("");
+            } while (linea != "ayuda");
+
+            Console.WriteLine("Has ingresado: \t" + linea);
         }
     }
 }
